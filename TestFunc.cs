53f0eab83dcd8c836dacfb1ccb7c2af3f7e6f2cc
Andrea <andre.kiba@gmail.com>
2019-03-17 18:35:51 +0100

    update test func

---
+++ TestFunc.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Newtonsoft.Json;
+
+namespace KLabSkill
+{
+    public class TestFunc
+    {
+        [FunctionName("TestFunc")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req)
+        {
+            string name = req.Query["name"];
+
+            var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+    }
+}
